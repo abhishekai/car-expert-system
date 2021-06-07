b39937dd50dc58fc8fb8a8dbb5817eb331fa36b1
Abhishek Srivastava <sabhi.shweta@gmail.com>
2021-06-07 23:05:30 +0530

    Issue node defined

--- troubleshooting-issues/src/com/ai/IssueNode.java
+++ troubleshooting-issues/src/com/ai/IssueNode.java
@@ -1,4 +1,30 @@
 package com.ai;
 
+/**
+ * Author:Abhishek Srivastava
+ * Class IssueNode : Represents the issue a car can have, and way to move further for an appropriate response
+ */
 public class IssueNode {
+
+    private String issue;
+
+    public IssueNode YES; // To move towards left in the DecisionTree
+    public IssueNode NO; // To move towards right in the DecisionTree
+
+    //Constructor for only response
+    public IssueNode(String issue){
+        this(issue, null, null);
+    }
+
+    //Constructor for issue and its sub nodes
+    public IssueNode(String issue, IssueNode yes, IssueNode no) {
+        this.issue = issue;
+        this.YES = yes;
+        this.NO = no;
+    }
+
+    public String toString(){
+        return issue;
+    }
+
 }
